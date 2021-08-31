0f65503aceface18487c3b018c5245abc5647e04
UyDung <dungnguyendinh911@gmail.com>
2021-08-31 14:11:23 +0700

    updata 31.8.21

--- script/script.js
+++ script/script.js
@@ -1,11 +1,33 @@
 const search_btn = document.getElementById('search-btn');
-const search__menu = document.getElementById('search__menu');
+const search_menu = document.querySelector('.dropdown-menu')
+const  closeSearchForm = document.getElementById('close-search-form');
+const btnCart = document.getElementById('btn-cart');
+const cart = document.getElementById('cart')
 
-search_btn.addEventListener('click', () => {
-     search__menu.style.visibility = 'visible';
-})
+// search_btn.addEventListener('click', () => {
+//      search_menu.style.visibility = 'visible';
+     
+// })
 
 const close_search_form = document.getElementById('close-search-form');
 close_search_form.addEventListener('click', () => {
-     search__menu.style.visibility = 'hidden';
+     search_menu.style.visibility = 'hidden';
+     
 })
+
+const  displayElement = (item) => {
+     let dropDown = item.parentElement.lastElementChild;
+     console.log(dropDown);
+     // dropDown.style.visibility = 'hidden';
+}
+
+const btnOptions = document.querySelectorAll('.btn-option');
+btnOptions.forEach((item) => {
+   
+   item.addEventListener('click', () => displayElement(item));
+      
+})
+
+
+
+
