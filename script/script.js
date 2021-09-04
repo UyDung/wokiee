226425b8194a51fe3666c8c85b04de3c3bde5cf2
UyDung <dungnguyendinh911@gmail.com>
2021-09-05 02:11:25 +0700

    update click outside dropdown close it

--- script/script.js
+++ script/script.js
@@ -20,34 +20,41 @@ var position = window.scrollY; /** position before search form close */
     const btnOptions = document.querySelectorAll(".btn-option");
     const closeSearchForm = document.getElementById("close-search-form");
     const searchForm = document.getElementById("search-form");
+    const listItems = document.querySelectorAll(".dropdown-menu");
 
     closeSearchForm.addEventListener("click", () => {
         searchForm.classList.remove("clicked");
         /* scroll to position that before search form close  */
         setTimeout(() => {
             window.scrollBy(0, position);
-            console.log(position);
         }, 2);
     });
 
     for (let item of btnOptions) {
         item.addEventListener("click", (item) => {
-            hiddenAllDropDown();
-            const direct = item.path[2].querySelector(".dropdown-menu")
-            direct.classList.toggle("clicked")
-            console.log(item);
-            console.log(direct);
+          //   hiddenAllDropDown();
+            const direct = item.path[2].querySelector(".dropdown-menu");
+            direct.classList.toggle("clicked");
+            direct.parentNode.addEventListener("click", (e) => {
+                e.stopPropagation();
+            });
+         
         });
     }
 
     const hiddenAllDropDown = () => {
-        const listItems = document.querySelectorAll(".dropdown-menu");
         for (let item of listItems) {
-            item.classList.remove("active");
+            item.classList.remove("clicked");
         }
     };
-}
 
+    //close Options menu
+    document.addEventListener("click", (e) => {
+        for (let item of listItems) {
+            item.classList.remove("clicked");
+        }
+    });
+}
 /**** showcase slide image */
 {
     const slides = document.querySelectorAll(".slide");
