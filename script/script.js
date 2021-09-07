57ffce381dfe15dc9026b85684ab005d89baa878
UyDung <dungnguyendinh911@gmail.com>
2021-09-07 14:50:03 +0700

    updata menu 7.9.21

--- script/script.js
+++ script/script.js
@@ -31,18 +31,19 @@ var position = window.scrollY; /** position before search form close */
     });
 
     for (let item of btnOptions) {
-        item.addEventListener("click", (item) => {
-          //   hiddenAllDropDown();
+        item.addEventListener('click', hiddenAllDropDown());
+
+        item.addEventListener("click", (item) => {                
             const direct = item.path[2].querySelector(".dropdown-menu");
             direct.classList.toggle("clicked");
+
             direct.parentNode.addEventListener("click", (e) => {
                 e.stopPropagation();
             });
-         
         });
     }
 
-    const hiddenAllDropDown = () => {
+    function hiddenAllDropDown(){
         for (let item of listItems) {
             item.classList.remove("clicked");
         }
@@ -59,20 +60,68 @@ var position = window.scrollY; /** position before search form close */
 {
     const slides = document.querySelectorAll(".slide");
     const slideText = document.querySelectorAll(".slide__text");
+    const navItems = document.querySelectorAll(".nav-item");
+
     var current = 0;
 
+    // automatic slide
     setInterval(() => {
         reset();
-        slides[current].classList.add("active");
+        slideAuto();
         current++;
         if (current === slides.length) {
             current = 0;
         }
+        console.log("current is " + current);
     }, 10000);
 
     const reset = () => {
         slides.forEach((item) => {
             item.classList.remove("active");
         });
+
+        for (let item of navItems) {
+            item.classList.remove("active");
+        }
     };
+
+    const slideAuto = () => {
+        slides[current].classList.add("active");
+        navItems[current].classList.add("active");
+    };
+
+    // manual slider click
+    for (let item of navItems) {
+        item.addEventListener("click", () => {
+            setTimeout(() => {
+               //  clearInterval();
+                //  current--;
+            }, 3000);
+
+            const currentID = item.id.charAt(item.id.length - 1);
+            for (let i = 0; i < slides.length; i++) {
+                if (i === currentID - 1) {
+                    slides[i].classList.add("active");
+                    navItems[i].classList.add("active");
+                } else {
+                    slides[i].classList.remove("active");
+                    navItems[i].classList.remove("active");
+                }
+            }
+        });
+    }
+
+    // arrow slide click
+    const arrowLeft = document.querySelector(".arrow-slide-left");
+    const arrowRight = document.querySelector(".arrow-slide-right");
+
+    arrowLeft.addEventListener("click", () => {
+        setTimeout(() => {
+            clearInterval();
+        }, 3000);
+
+        console.log(current);
+    });
+
+    arrowRight.addEventListener("click", () => {});
 }
